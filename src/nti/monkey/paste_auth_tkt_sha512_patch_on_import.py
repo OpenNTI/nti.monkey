05d1ef1f514591823d5f08ce934ed6e199cf3b49
Jason Madden <jason.madden@nextthought.com>
2013-01-30 20:18:47 +0000

    Update for compatibility with repoze.who 2.1 which features better pypy and python3 support.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@15477 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/monkey/paste_auth_tkt_sha512_patch_on_import.py
+++ src/nti/monkey/paste_auth_tkt_sha512_patch_on_import.py
@@ -5,6 +5,9 @@ Adapts :mod:`paste.auth.auth_tkt` to use SHA512 cookies instead of
 MD5 cookies. This is a change inspired by Pyramid 1.4, which did
 something similar in its own version of :class:`pyramid.authentication.AuthTkt`.
 
+The :mod:`repoze.who` system relies on Paste in 2.0, but no longer
+does in 2.1, so this also performs cleanup work in that module.
+
 $Id$
 """
 
@@ -31,15 +34,29 @@ def _parse_ticket( s, t, ip ):
 		raise paste.auth.auth_tkt.BadTicket( e.message, e.expected )
 
 def patch():
-	try:
-		if paste.auth.auth_tkt.md5 == md5:
-			# We only have to change these here...
-			paste.auth.auth_tkt.md5 = sha512
-			paste.auth.auth_tkt.parse_ticket = _parse_ticket
+	def _patch( mod ):
+		if mod.md5 == md5:
+			mod.md5 = sha512
+			mod.parse_ticket = _parse_ticket
 
-			# ...because repoze imports the module
+	try:
+		from repoze.who import _auth_tkt as who_auth_tkt
+		# Match the exceptions
+		paste.auth.auth_tkt.BadTicket = who_auth_tkt.BadTicket
+		_patch( who_auth_tkt )
+	except ImportError:
+		who_auth_tkt = None
+
+	try:
+		# We only have to change these here...
+		_patch( paste.auth.auth_tkt )
+		# ...because repoze imports the module (in 2.0)
+		if who_auth_tkt is None:
 			assert repoze.who.plugins.auth_tkt.auth_tkt == paste.auth.auth_tkt
-			assert 'parse_ticket' not in repoze.who.plugins.auth_tkt.__dict__
+		else: # 2.1
+			assert repoze.who.plugins.auth_tkt.auth_tkt == who_auth_tkt
+
+		assert 'parse_ticket' not in repoze.who.plugins.auth_tkt.__dict__
 	except AttributeError:
 		raise ImportError( "Paste does not use MD5 anymore. Incompatible change. FIXME" )
 
--- src/nti/monkey/tests/test_paste_auth_tkt_sha512_patch_on_import.py
+++ src/nti/monkey/tests/test_paste_auth_tkt_sha512_patch_on_import.py
@@ -18,6 +18,7 @@ from hamcrest import assert_that
 from hamcrest import is_
 from hamcrest import has_key
 from hamcrest import has_entry
+import unittest
 
 from nose.tools import assert_raises
 
@@ -25,14 +26,7 @@ import nti.tests
 
 from hashlib import sha512
 
-def test_patch():
-	import paste.auth.auth_tkt
-	import nti.monkey.paste_auth_tkt_sha512_patch_on_import
-	nti.monkey.paste_auth_tkt_sha512_patch_on_import.patch()
-
-	assert_that( paste.auth.auth_tkt.md5, is_( sha512 ) )
-
-	from paste.auth.auth_tkt import AuthTicket, parse_ticket
+def _do_test_parse(AuthTicket, parse_ticket, BadTicket):
 
 	secret = b'secret'
 	time = 1234
@@ -44,6 +38,37 @@ def test_patch():
 	assert_that( parse_ticket( secret, cookie_value, ip ),
 				 is_( (time, user, [''], '' ) ) )
 
-	with assert_raises(paste.auth.auth_tkt.BadTicket):
+	with assert_raises(BadTicket):
 		cookie_value = cookie_value.replace( 'a', 'b' )
 		parse_ticket( secret, cookie_value, ip )
+
+def test_patch():
+	import paste.auth.auth_tkt
+	import nti.monkey.paste_auth_tkt_sha512_patch_on_import
+	nti.monkey.paste_auth_tkt_sha512_patch_on_import.patch()
+
+	assert_that( paste.auth.auth_tkt.md5, is_( sha512 ) )
+
+	from paste.auth.auth_tkt import AuthTicket, parse_ticket
+
+	_do_test_parse( paste.auth.auth_tkt.AuthTicket,
+					paste.auth.auth_tkt.parse_ticket,
+					paste.auth.auth_tkt.BadTicket )
+
+try:
+	import repoze.who._auth_tkt as is_repoze_21
+	is_repoze_20 = False
+except ImportError:
+	is_repoze_21 = False
+	is_repoze_20 = True
+
+@unittest.skipIf(is_repoze_20, "Only needed under repoze.who 2.1" )
+def test_patch_repoze21():
+	import nti.monkey.paste_auth_tkt_sha512_patch_on_import
+	nti.monkey.paste_auth_tkt_sha512_patch_on_import.patch()
+
+	assert_that( is_repoze_21.md5, is_( sha512 ) )
+
+	_do_test_parse( is_repoze_21.AuthTicket,
+					is_repoze_21.parse_ticket,
+					is_repoze_21.BadTicket )
