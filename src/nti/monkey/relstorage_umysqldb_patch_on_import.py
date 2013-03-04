73b95a07f0c1361d2f36897ff4e3fec07c17dd57
Jason Madden <jason.madden@nextthought.com>
2013-03-04 15:58:11 +0000

    Patch bug in relstorage related to assumption about persistent.timestamp and its repr.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@16606 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/monkey/relstorage_umysqldb_patch_on_import.py
+++ src/nti/monkey/relstorage_umysqldb_patch_on_import.py
@@ -10,7 +10,22 @@ $Id$
 from __future__ import print_function, unicode_literals, absolute_import
 __docformat__ = "restructuredtext en"
 
-
+def _patch_relstorage_for_newer_persistent():
+	from persistent.timestamp import pyTimeStamp
+	# The independent release of persistent changes repr(TimeStamp) to
+	# return an actual repr of its raw data, not the raw data. This
+	# is required for py3, but breaks RelStorage 1.5's assumption that
+	# the repr of a timestamp is its raw data.
+	# A search of the source code reveals this to only be used in relstorage.storage
+	# Depending on whether the c extensions are in play, TimeStamp may
+	# be a function or a type...if it is a type, we cannot actually
+	# get to it reliably by name
+	def _repr(o):
+		if isinstance(o, pyTimeStamp) or (type(o).__name__ == 'TimeStamp' and type(o).__module__ == 'persistent'):
+			return o.raw()
+		return repr(o)
+	import relstorage.storage
+	relstorage.storage.repr = _repr
 
 def _patch():
 	import umysqldb
@@ -77,6 +92,7 @@ def _patch():
 		# disconnected_exceptions: "indicates the connection is disconnected"
 		attr.disconnected_exceptions += (IOError,) # This one can escape mapping; note we don't make pymysql.err.Error indicate disconnection
 
+	_patch_relstorage_for_newer_persistent()
 _patch()
 
 def patch():
---
+++ src/nti/monkey/tests/test_relstorage_umysqldb_patch_on_import.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+
+
+$Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+#disable: accessing protected members, too many methods
+#pylint: disable=W0212,R0904
+
+
+from hamcrest import assert_that
+from hamcrest import is_
+from hamcrest import has_key
+from hamcrest import has_entry
+
+import nti.tests
+
+def test_timestamp_to_tid_patch():
+	import nti.monkey.relstorage_umysqldb_patch_on_import
+	nti.monkey.relstorage_umysqldb_patch_on_import.patch()
+
+	from relstorage.storage import RelStorage
+
+	class Adapter(object):
+		pass
+
+	class Schema(object):
+		def prepare(self): pass
+
+	class Locker(object):
+		def hold_commit_lock( self, cursor, ensure_current=True ):
+			pass
+
+	class TxnControl(object):
+		last_tid = 1
+		def get_tid( self, cursor ):
+			return self.last_tid
+		def add_transaction( self, cursor, tid_int, user, desc, ext ):
+			pass
+
+	adapter = Adapter()
+	adapter.schema = Schema()
+	adapter.txncontrol = TxnControl()
+	adapter.locker = Locker()
+
+	storage = RelStorage( adapter )
+	storage._transaction = object()
+	storage._ude = (None, None, None)
+
+	storage._prepare_tid()
+
+	assert_that( storage._tid, is_( str ) ) # bytes, not unicode
