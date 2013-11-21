aecc93ad7e35c982f7c0a39471fbd469ac0ae7d1
Jason Madden <jason.madden@nextthought.com>
2013-11-21 15:59:59 +0000

    Need the umysqldb patch too.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@27286 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/monkey/nti_zodbconvert.py
+++ src/nti/monkey/nti_zodbconvert.py
@@ -10,7 +10,7 @@ $Id$
 from __future__ import print_function, unicode_literals, absolute_import
 __docformat__ = "restructuredtext en"
 
-
+from nti.monkey import relstorage_umysqldb_patch_on_import
 from nti.monkey import relstorage_timestamp_repr_patch_on_import
 from nti.monkey import relstorage_zlibstorage_patch_on_import
 
