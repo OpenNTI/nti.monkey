c8e3710730d6397b1bc34caa2810026daea6990b
Jason Madden <jason.madden@nextthought.com>
2013-12-11 12:57:01 +0000

    Bump pyramid/webob which brings in some deprecation warnings to fix later but all test pass after accounting for the cookie fixes. Bump z3c.rml but we don't use the changed feature at this time. Bump z3c.recipe.staticlxml which improves linux and patch support but did not trigger a rebuild.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@27968 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/monkey/webob_cookie_escaping_patch_on_import.py
+++ src/nti/monkey/webob_cookie_escaping_patch_on_import.py
@@ -3,14 +3,14 @@
 """
 Versions of :mod:`webob.cookie` up through at least 1.2.3 are overly
 aggressive when it comes to escaping the values of the Set-Cookie
-header.
+header. This is fixed in 1.3 so this patch does nothing there.
 
 According to `RFC 6265 <http://tools.ietf.org/html/rfc6265#section-4.1.1>`, only a few values
 have to be escaped: control characters, double quote, comma, semicolon, and backslash. Notably, the at-sign, @, is
 allowed. However, webob leaves this on its list.
 
-Importing this module (and calling patch) adjusts the webob cookie functions to not
-escape these values.
+Importing this module (and calling patch) adjusts the webob cookie
+functions to not escape these values.
 
 $Id$
 """
@@ -18,33 +18,15 @@ $Id$
 from __future__ import print_function, unicode_literals, absolute_import
 __docformat__ = "restructuredtext en"
 
-from webob import cookies as _cookies
-from webob.cookies import _quote # Raise ImportError if the internals have changed
+import pkg_resources
 
-
-# First, see if it needs to be escaped
-if _quote( b'foo@bar' ) != 'foo@bar':
-	# Again, raise ImportError if the internals have changed
-	try:
-		getattr( _cookies, '_no_escape_special_chars' )
-		getattr( _cookies, '_no_escape_chars' )
-		getattr( _cookies, '_no_escape_bytes' )
-	except AttributeError:
-		raise ImportError("WebOb internals have changed")
-
-	_cookies._no_escape_special_chars = _cookies._no_escape_special_chars + b'@'
-	_cookies._no_escape_chars = _cookies._no_escape_chars + b'@'
-	_cookies._no_escape_bytes = _cookies._no_escape_bytes + b'@'
-
-	# Safe to leave the maps alone
-
-	try:
-		assert _quote( b'foo@bar' ) == 'foo@bar'
-	except AssertionError:
-		raise ImportError( "Unable to patch WebOb correctly" )
-else:
-	raise ImportError("WebOb internals have changed; review this patch")
-
+# In fact we require running on 1.3, which changes
+# the implementation...sadly, the __version__ attribute
+# did not change from 1.2.3 to 1.3. So we use
+# the brute-force method to check
+dist = pkg_resources.get_distribution('webob')
+if not dist.version or dist.version < '1.3':
+	raise ImportError("WebOb less that 1.3 is no longer supported: %s" % dist )
 
 def patch():
 	pass
