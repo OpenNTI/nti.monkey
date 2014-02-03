fc5a579fbbd4c745e28d7c48d603a105320c64dd
Jason Madden <jason.madden@nextthought.com>
2014-02-03 19:51:56 +0000

    Stop forcing umylqdb in nti_mulit_zodb_* since some databases really need MySQLdb
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@32093 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/monkey/nti_multi_zodb_check_refs.py
+++ src/nti/monkey/nti_multi_zodb_check_refs.py
@@ -22,12 +22,10 @@ from __future__ import print_function, unicode_literals, absolute_import
 __docformat__ = "restructuredtext en"
 
 
-from nti.monkey import gevent_patch_on_import
 from nti.monkey import relstorage_timestamp_repr_patch_on_import
 from nti.monkey import relstorage_zlibstorage_patch_on_import
 from nti.monkey import relstorage_explicitly_close_memcache_patch_on_import
 
-gevent_patch_on_import.patch()
 relstorage_timestamp_repr_patch_on_import.patch()
 relstorage_zlibstorage_patch_on_import.patch()
 relstorage_explicitly_close_memcache_patch_on_import.patch()
