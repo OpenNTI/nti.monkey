OpenNTI/nti.monkey

commit c4cbd1ad77f4e44e0a2d4d574ff2d3751c63e697

clean up; add division
git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@50115 3dfb626b-079f-4ab1-a9f6-4654e5377508

src/nti/monkey/gevent_patch_on_import.py
@@ -7,11 +7,13 @@ hence the unwieldy name.
 
 If this is imported too late and we know that things will not work, we raise an exception.
 
-$Id$
+.. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import
+from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
 
 # All the patching uses private things so turn that warning off
 #pylint: disable=W0212

src/nti/monkey/nti_multi_zodb_check_refs.py
@@ -15,22 +15,24 @@ replace its dependency on that with the new :mod:`zodbpickle` package (and
 even then its not clear this would work; it seems that noload is only
 available in the C code, not the python version).
 
-$Id$
+.. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import
+from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
 
-from . import relstorage_patch_all_except_gevent_on_import
+logger = __import__('logging').getLogger(__name__)
 
+from . import relstorage_patch_all_except_gevent_on_import
 relstorage_patch_all_except_gevent_on_import.patch()
 
 from . import python_persistent_bugs_patch_on_import
 python_persistent_bugs_patch_on_import.patch()
 
 # See extensive comments.
-from nti.monkey.nti_multi_zodb_gc import fixrefs
 from nti.monkey.nti_multi_zodb_gc import report
+from nti.monkey.nti_multi_zodb_gc import fixrefs
+
 import sys
 from pkg_resources import load_entry_point
 
@@ -40,7 +42,6 @@ def main():
 		ec = load_entry_point('zc.zodbdgc', 'console_scripts', 'multi-zodb-check-refs')()
 	finally:
 		report()
-
 	sys.exit(ec)
 
 if __name__ == '__main__':

src/nti/monkey/nti_runzeo.py
@@ -13,31 +13,31 @@ disconnections better---fewer ``<class '_mysql_exceptions.OperationalError'>: (2
 We do make relstorage work with newer releases of
 persistent and make it compatible with zlibstorage.
 
-$Id$
+.. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import
+from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
 
+logger = __import__('logging').getLogger(__name__)
+
 from nti.monkey import relstorage_umysqldb_patch_on_import
-from nti.monkey import relstorage_timestamp_repr_patch_on_import
 from nti.monkey import relstorage_zlibstorage_patch_on_import
+from nti.monkey import relstorage_timestamp_repr_patch_on_import
 from nti.monkey import relstorage_explicitly_close_memcache_patch_on_import
 
 relstorage_umysqldb_patch_on_import.patch()
-relstorage_timestamp_repr_patch_on_import.patch()
 relstorage_zlibstorage_patch_on_import.patch()
+relstorage_timestamp_repr_patch_on_import.patch()
 relstorage_explicitly_close_memcache_patch_on_import.patch()
 
 from . import python_persistent_bugs_patch_on_import
 python_persistent_bugs_patch_on_import.patch()
 
-
 # NOTE: Not importing patch_all, don't want gevent
 
 import sys
 from pkg_resources import load_entry_point, get_distribution
-
 
 def main():
 	zeo_dist = get_distribution( 'ZEO' )

src/nti/monkey/nti_zodbconvert.py
@@ -4,23 +4,22 @@
 A hack to help us ensure that we are loading and monkey-patching
 the desired parts of zodbconvert before it gets started.
 
-$Id$
+.. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import
+from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
 
-from nti.monkey import relstorage_patch_all_on_import
+logger = __import__('logging').getLogger(__name__)
 
+from nti.monkey import relstorage_patch_all_on_import
 relstorage_patch_all_on_import.patch()
 
 from . import python_persistent_bugs_patch_on_import
 python_persistent_bugs_patch_on_import.patch()
 
-
 import sys
 from pkg_resources import load_entry_point
-
 
 def main():
 	sys.exit(

src/nti/monkey/__init__.py
@@ -5,8 +5,11 @@ This package contains modules that monkey-patch various parts of the system. Typ
 will do so on import (specified in the name) but will also provide a 'patch' function to avoid
 "unused import" warnings. Each module will have minimal dependencies so it can be imported as early as possible.
 
-$Id$
+.. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import
+from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+

src/nti/monkey/paste_auth_tkt_sha512_patch_on_import.py
@@ -8,20 +8,23 @@ something similar in its own version of :class:`pyramid.authentication.AuthTkt`.
 The :mod:`repoze.who` system relies on Paste in 2.0, but no longer
 does in 2.1, so this also performs cleanup work in that module.
 
-$Id$
+.. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import
+from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
 
+logger = __import__('logging').getLogger(__name__)
 
 import functools
 
-from hashlib import sha512
 from hashlib import md5
+from hashlib import sha512
 
 import paste.auth.auth_tkt
+
 import repoze.who.plugins.auth_tkt
+
 from pyramid.authentication import parse_ticket as _pyramid_parse, BadTicket as _pyramid_BadTicket
 
 @functools.wraps(_pyramid_parse)

commit 40adbb7ed79085d534fe4b422820041ed7d17c64

All the bugs that the python_persistent monkey patch was working around have been fixed and released, so remove it. Also fix a gevent lock problem introduced in r50115 that impacted logging.
git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@64522 3dfb626b-079f-4ab1-a9f6-4654e5377508

src/nti/monkey/gevent_patch_on_import.py
@@ -13,7 +13,9 @@ If this is imported too late and we know that things will not work, we raise an 
 from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
 
-logger = __import__('logging').getLogger(__name__)
+# DO NOT create a logger at the top of this file;
+# it allocates locks
+#logger = __import__('logging').getLogger(__name__)
 
 # All the patching uses private things so turn that warning off
 # pylint: disable=W0212
@@ -27,12 +29,12 @@ def _patch_process_pool_executor():
 
 	import multiprocessing
 	import concurrent.futures
-	
+
 	# Stash the original
 	setattr(concurrent.futures,
-			'_ProcessPoolExecutor', 
+			'_ProcessPoolExecutor',
 			concurrent.futures.ProcessPoolExecutor )
-	
+
 	def ProcessPoolExecutor( max_workers=None ):
 		if max_workers is None:
 			max_workers = multiprocessing.cpu_count()
@@ -94,10 +96,10 @@ def _patch_logging():
 	# thread/threadName, process (aka pid) and processName attributes.
 
 	class _LogRecord(LogRecord):
-		
+
 		def __init__( self, *args, **kwargs ):
 			LogRecord.__init__( self, *args, **kwargs )
-			
+
 			if not self.threadName:
 				# logger.logThreads was set to False probably
 				return
@@ -147,9 +149,9 @@ def _patch_memcache():
 	finally:
 		threading.local = local
 
-def check_threadlocal_status(names=('transaction.ThreadTransactionManager', 
+def check_threadlocal_status(names=('transaction.ThreadTransactionManager',
 									'zope.component.hooks.siteinfo',
-									'pyramid.threadlocal.ThreadLocalManager', 
+									'pyramid.threadlocal.ThreadLocalManager',
 									'pyramid.threadlocal.manager')):
 	# depending on the order of imports, we may need to patch
 	# some things up manually.
@@ -158,7 +160,7 @@ def check_threadlocal_status(names=('transaction.ThreadTransactionManager',
 	# so if they aren't patched due to a bad import order, we
 	# bail
 	import zope.dottedname.resolve as dottedname
-	
+
 	for name in names:
 		try:
 			obj = dottedname.resolve(name)
@@ -179,19 +181,21 @@ def check_threadlocal_status(names=('transaction.ThreadTransactionManager',
 	if db.threading.RLock is not gevent.lock.RLock:
 		raise TypeError("ZODB.DB/threading.RLock not monkey patched")
 
-
+# XXX: This is fixed in https://github.com/gevent/gevent/pull/546,
+# https://github.com/gevent/gevent/pull/551 and 552. We should switch
+# to one of those branches (552 is required for pypy.)
 def new_ssl_init():
-	
+
 	import errno
-	
+
 	from ssl import SSLContext
 	from ssl import SOL_SOCKET, SO_TYPE
 	from ssl import AF_INET, SOCK_STREAM
-	
+
 	from gevent.ssl import CERT_NONE
 	from gevent.ssl import PROTOCOL_SSLv23
 	from gevent.socket import socket, error as socket_error
-	
+
 	_delegate_methods = ('recv', 'recvfrom', 'recv_into', 'recvfrom_into', 'send', 'sendto')
 
 	def sslsock_init(self, sock=None, keyfile=None, certfile=None,
@@ -289,13 +293,22 @@ def new_ssl_init():
 	return sslsock_init
 
 version_info = getattr( gevent, 'version_info', (0, 0, 0, 'final', 0))
-if version_info[0:3] == (1, 0, 1) and sys.version_info[0:3] >= (2,7,9):
-	logger.info( "Monkey-patching the SSLSocket" )
-	from gevent.ssl import SSLSocket
-	SSLSocket.__init__ = new_ssl_init()
-	
+
+def _patch_ssl():
+	import platform
+	is_pypy = platform.python_implementation() == 'PyPy'
+	if is_pypy:
+		# If we're running this on pypy, we have a fixed branch
+		# anyway
+		return
+
+	if version_info[0:3] == (1, 0, 1) and sys.version_info[0:3] >= (2,7,9):
+		logger.info( "Monkey-patching the SSLSocket" )
+		from gevent.ssl import SSLSocket
+		SSLSocket.__init__ = new_ssl_init()
+
 # Don't do this when we are loaded for conflict resolution into somebody else's space
-if version_info[0] >= 1 and 'ZEO' not in sys.modules: 
+if version_info[0] >= 1 and 'ZEO' not in sys.modules:
 
 	# As of 2012-10-30 and gevent 1.0rc1, the change in 1.0b4 to patch os.read and os.write
 	# is undone. Comments below left for historical interest
@@ -319,7 +332,7 @@ if version_info[0] >= 1 and 'ZEO' not in sys.modules:
 	# 	logger.exception( "Failed to remove os.read/write patch. Gevent outdated?")
 	# 	raise
 
-	# As of 2012-10-06, patching sys/std[out/err/in] hangs gunicorn, so be sure it's false 
+	# As of 2012-10-06, patching sys/std[out/err/in] hangs gunicorn, so be sure it's false
 	# (this is marked experimental in 1.0rc1)
 	gevent.monkey.patch_all(subprocess=True, sys=False, Event=False)
 
@@ -382,6 +395,8 @@ if version_info[0] >= 1 and 'ZEO' not in sys.modules:
 
 	_patch_memcache()
 
+	_patch_ssl()
+
 	if TRACE_GREENLETS:
 		import greenlet
 		def greenlet_trace( event, origin ):
@@ -405,9 +420,6 @@ if version_info[0] >= 1 and 'ZEO' not in sys.modules:
 	# Monkey-patch for RelStorage to use pure-python drivers that are non-blocking
 	from . import relstorage_umysqldb_patch_on_import
 	relstorage_umysqldb_patch_on_import.patch()
-
-	from . import python_persistent_bugs_patch_on_import
-	python_persistent_bugs_patch_on_import.patch()
 
 else:
 	logger = __import__('logging').getLogger(__name__)

src/nti/monkey/nti_multi_zodb_check_refs.py
@@ -26,9 +26,6 @@ logger = __import__('logging').getLogger(__name__)
 from . import relstorage_patch_all_except_gevent_on_import
 relstorage_patch_all_except_gevent_on_import.patch()
 
-from . import python_persistent_bugs_patch_on_import
-python_persistent_bugs_patch_on_import.patch()
-
 # See extensive comments.
 from nti.monkey.nti_multi_zodb_gc import report
 from nti.monkey.nti_multi_zodb_gc import fixrefs

src/nti/monkey/nti_multi_zodb_gc.py
@@ -73,6 +73,9 @@ logger = __import__('logging').getLogger(__name__)
 # [oid]
 # 	A persistent weak reference
 
+# XXX: This is fixed in https://github.com/zopefoundation/zc.zodbdgc/pull/1.
+# We should just switch to that branch everywhere.
+
 try:
 	from zodbpickle.fastpickle import Unpickler
 except ImportError: # PyPy?

src/nti/monkey/nti_runzeo.py
@@ -31,10 +31,8 @@ relstorage_zlibstorage_patch_on_import.patch()
 relstorage_timestamp_repr_patch_on_import.patch()
 relstorage_explicitly_close_memcache_patch_on_import.patch()
 
-from . import python_persistent_bugs_patch_on_import
-python_persistent_bugs_patch_on_import.patch()
-
 # NOTE: Not importing patch_all, don't want gevent
+# (XXX: Except we probably do, definitely under pypy.)
 
 import sys
 from pkg_resources import load_entry_point, get_distribution

src/nti/monkey/nti_zodbconvert.py
@@ -15,9 +15,6 @@ logger = __import__('logging').getLogger(__name__)
 from nti.monkey import relstorage_patch_all_on_import
 relstorage_patch_all_on_import.patch()
 
-from . import python_persistent_bugs_patch_on_import
-python_persistent_bugs_patch_on_import.patch()
-
 import sys
 from pkg_resources import load_entry_point
 

src/nti/monkey/python_persistent_bugs_patch_on_import.py
@@ -1,81 +1,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""
-Patches some bugs found in the pure-python versions of persistent.
-
-.. $Id$
-"""
-
-from __future__ import print_function, unicode_literals, absolute_import, division
-__docformat__ = "restructuredtext en"
-
-logger = __import__('logging').getLogger(__name__)
-
-# The current version of invalidate does not correctly
-# move forward during invalidation if the value is not
-# the first item in the ring
-# https://github.com/zopefoundation/persistent/pull/4
-
-from persistent.interfaces import GHOST
-from persistent.picklecache import PickleCache
-
-def _invalidate(self, oid):
-	value = self.data.get(oid)
-	if value is not None and value._p_state != GHOST:
-		value._p_invalidate()
-		node = self.ring.next
-		while node is not self.ring:
-			if node.object is value:
-				node.prev.next, node.next.prev = node.next, node.prev
-				break
-			node = node.next # JAM: Addition
-	elif oid in self.persistent_classes:
-		del self.persistent_classes[oid]
-
-PickleCache._invalidate = _invalidate
-
-# There is a bad interaction with _p_accessed setting
-# the MRU time in the picklecache during initial
-# serialiaztion: the new object isn't in the picklecache
-# yet!
-# https://github.com/zopefoundation/persistent/pull/6
-from persistent.persistence import Persistent
-_orig_p_accessed = Persistent._p_accessed
-
-def _p_accessed(self):
-	try:
-		_orig_p_accessed(self)
-	except KeyError:
-		pass
-
-Persistent._p_accessed = _p_accessed
-
-# Persistent in 4.0.6 also has a problem setting the
-# state if the dictionary has exactly two keys
-# https://github.com/zopefoundation/persistent/pull/5
-_orig_setstate = Persistent.__setstate__
-
-def __setstate__(self, state):
-	if isinstance(state, dict):
-		state = state, ()
-	_orig_setstate(self, state)
-
-Persistent.__setstate__ = __setstate__
-
-# In pypy (and possibly in zodbpickle) under python2,
-# ZODB 4's ObjectWriter assumes it's working with cPickle
-# and sets the wrong attribute to get the
-# persistent ids.
-# https://github.com/zopefoundation/ZODB/pull/17
-import cPickle
-
-from nti.common.property import alias
-
-if 	hasattr(cPickle.Pickler, 'persistent_id') and \
-	not hasattr(cPickle.Pickler, 'inst_persistent_id'):
-	cPickle.Pickler.inst_persistent_id = alias('persistent_id')
-
-def patch():
-	pass
-
-patch()

src/nti/monkey/relstorage_patch_all_except_gevent_on_import.py
@@ -13,16 +13,12 @@ from nti.monkey import relstorage_external_gc_patch_on_import
 from nti.monkey import relstorage_explicitly_close_memcache_patch_on_import
 from nti.monkey import relstorage_locker_patch_on_import
 
-from nti.monkey import python_persistent_bugs_patch_on_import
-
 def patch():
 	relstorage_timestamp_repr_patch_on_import.patch()
 	relstorage_zlibstorage_patch_on_import.patch()
 	relstorage_external_gc_patch_on_import.patch()
 	relstorage_explicitly_close_memcache_patch_on_import.patch()
 	relstorage_locker_patch_on_import.patch()
-
-	python_persistent_bugs_patch_on_import.patch()
 
 	try:
 		import MySQLdb

src/nti/monkey/tests/test_python_persistent_bugs.py
@@ -1,25 +1,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from __future__ import print_function, absolute_import, division
-__docformat__ = "restructuredtext en"
-
-# disable: accessing protected members, too many methods
-# pylint: disable=W0212,R0904
-
-from hamcrest import is_
-from hamcrest import assert_that
-
-def test_patch():
-	from .. import python_persistent_bugs_patch_on_import
-	python_persistent_bugs_patch_on_import.patch()
-
-	from persistent.persistence import Persistent
-	class Derived(Persistent):
-		pass
-
-	inst = Derived()
-	inst.x = 1
-	inst.__setstate__( {'k': 1, 'v': 2 })
-
-	assert_that( inst.__dict__, is_({'k': 1, 'v': 2 }))