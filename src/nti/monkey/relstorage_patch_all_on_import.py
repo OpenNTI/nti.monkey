cd0e41a4db25ed4ac441bed692077230524c573d
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-05-14 19:18:09 +0000

    add plonefile_zopefile_patch_on_import
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@65339 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/monkey/relstorage_patch_all_on_import.py
+++ src/nti/monkey/relstorage_patch_all_on_import.py
@@ -11,6 +11,7 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
+from nti.monkey import plonefile_zopefile_patch_on_import
 from nti.monkey import gevent_patch_on_import
 from nti.monkey import relstorage_umysqldb_patch_on_import
 from nti.monkey import relstorage_timestamp_repr_patch_on_import
@@ -24,6 +25,7 @@ relstorage_zlibstorage_patch_on_import.patch()
 relstorage_umysqldb_patch_on_import.patch()
 relstorage_explicitly_close_memcache_patch_on_import.patch()
 relstorage_locker_patch_on_import.patch()
+plonefile_zopefile_patch_on_import.patch()
 
 def patch():
 	pass
