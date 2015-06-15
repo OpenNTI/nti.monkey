fd1574885e7edd7b6385abdebabed80e73127f5e
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-06-15 22:02:24 +0000

    add helper module
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@67244 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/monkey/relstorage_patch_all_except_gevent_on_import.py
+++ src/nti/monkey/relstorage_patch_all_except_gevent_on_import.py
@@ -7,11 +7,12 @@
 from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
 
-from nti.monkey import relstorage_timestamp_repr_patch_on_import
-from nti.monkey import relstorage_zlibstorage_patch_on_import
-from nti.monkey import relstorage_external_gc_patch_on_import
-from nti.monkey import relstorage_explicitly_close_memcache_patch_on_import
+from nti.monkey import nti_internal_patch_on_import
 from nti.monkey import relstorage_locker_patch_on_import
+from nti.monkey import relstorage_external_gc_patch_on_import
+from nti.monkey import relstorage_zlibstorage_patch_on_import
+from nti.monkey import relstorage_timestamp_repr_patch_on_import
+from nti.monkey import relstorage_explicitly_close_memcache_patch_on_import
 
 def patch():
 	relstorage_timestamp_repr_patch_on_import.patch()
@@ -19,9 +20,10 @@ def patch():
 	relstorage_external_gc_patch_on_import.patch()
 	relstorage_explicitly_close_memcache_patch_on_import.patch()
 	relstorage_locker_patch_on_import.patch()
-
+	nti_internal_patch_on_import.patch()
+	
 	try:
-		import MySQLdb
+		__import__('MySQLdb')
 	except ImportError:
 		# This may or may not work.
 		from nti.monkey import relstorage_umysqldb_patch_on_import
