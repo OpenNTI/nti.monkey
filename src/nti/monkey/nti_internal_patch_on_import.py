318337a4a5d61df22472bb00353399ef1bea460f
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-06-16 01:49:44 +0000

    add some comments
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@67263 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/monkey/nti_internal_patch_on_import.py
+++ src/nti/monkey/nti_internal_patch_on_import.py
@@ -9,5 +9,11 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
+# from nti.common.deprecated import moved
+
 def patch():
+# 	moved("nti.dataserver.core", "nti.dataserver_core")
+# 	moved("nti.dataserver.core.interfaces", "nti.dataserver_core.interfaces")
+# 	moved("nti.dataserver.fragments", "nti.dataserver_fragments")
+# 	moved("nti.dataserver.fragments.interfaces", "nti.dataserver_fragments.interfaces")
 	pass
