0ab8b6e077945f8a89aea5a9cd948345ee93a273
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-07-20 17:20:39 +0000

    add reference for broken object
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@69248 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/monkey/nti_internal_patch_on_import.py
+++ src/nti/monkey/nti_internal_patch_on_import.py
@@ -12,7 +12,11 @@ logger = __import__('logging').getLogger(__name__)
 from nti.common.deprecated import moved
 
 def patch():
+	# deprecated core/fragments pkgs
 	moved("nti.dataserver.core", "nti.dataserver_core")
 	moved("nti.dataserver.core.interfaces", "nti.dataserver_core.interfaces")
 	moved("nti.dataserver.fragments", "nti.dataserver_fragments")
 	moved("nti.dataserver.fragments.interfaces", "nti.dataserver_fragments.interfaces")
+
+	# deleted session_consumer module
+	moved("nti.dataserver.session_consumer", "nti.socketio.session_consumer")
