8d3031c3001ea680c5167282485ff6a74a3c5d9a
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-11-29 21:10:11 +0000

    initial new search impl (breaking changes) lots to fix
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@101577 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/monkey/patch_nti_internal_on_import.py
+++ src/nti/monkey/patch_nti_internal_on_import.py
@@ -12,6 +12,20 @@ logger = __import__('logging').getLogger(__name__)
 from nti.common.deprecated import moved
 
 def patch():
+	# content search
+	moved(str("nti.contentsearch._content_utils"), str("nti.contentsearch._deprecated"))
+	moved(str("nti.contentsearch._discriminators"), str("nti.contentsearch._deprecated"))
+	moved(str("nti.contentsearch._indexmanager"), str("nti.contentsearch._deprecated"))
+	moved(str("nti.contentsearch._repoze_index"), str("nti.contentsearch._deprecated"))
+	moved(str("nti.contentsearch._whoosh_schemas"), str("nti.contentsearch._deprecated"))
+	moved(str("nti.contentsearch.content_types"), str("nti.contentsearch._deprecated"))
+	moved(str("nti.contentsearch.discriminators"), str("nti.contentsearch._deprecated"))
+	moved(str("nti.contentsearch.indexmanager"), str("nti.contentsearch._deprecated"))
+	moved(str("nti.contentsearch.repoze_adpater"), str("nti.contentsearch._deprecated"))
+	moved(str("nti.contentsearch.whoosh_schemas"), str("nti.contentsearch._deprecated"))
+	moved(str("nti.contentsearch.whoosh_searcher"), str("nti.contentsearch._deprecated"))
+	moved(str("nti.contentsearch.whoosh_storage"), str("nti.contentsearch._deprecated"))
+	
 	# deprecated core/fragments pkgs
 	moved(str("nti.dataserver.core"), str("nti.dataserver_core"))
 	moved(str("nti.dataserver.core.interfaces"), str("nti.dataserver_core.interfaces"))
@@ -21,6 +35,3 @@ def patch():
 
 	# deleted session_consumer module
 	moved(str("nti.dataserver.session_consumer"), str("nti.socketio.session_consumer"))
-	
-	# content search invalid package
-	moved(str("nti.contentsearch.repoze_adpater"), str("nti.contentsearch._repoze_adapter"))
