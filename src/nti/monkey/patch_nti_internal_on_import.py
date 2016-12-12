273bd708267be997c267c70ed8eed775ddc82728
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-12-12 17:21:58 +0000

    one more deprecated code removal
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver@102393 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/monkey/patch_nti_internal_on_import.py
+++ src/nti/monkey/patch_nti_internal_on_import.py
@@ -12,6 +12,9 @@ logger = __import__('logging').getLogger(__name__)
 from nti.common.deprecated import moved
 
 def patch():
+	# content library
+	moved(str("nti.appserver.contentlibrary_views"), str("nti.app.contentlibrary.content_unit_preferences.adapters"))
+	
 	# content search
 	moved(str("nti.contentsearch._content_utils"), str("nti.contentsearch._deprecated"))
 	moved(str("nti.contentsearch._discriminators"), str("nti.contentsearch._deprecated"))
