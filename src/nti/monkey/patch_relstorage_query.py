f8792f6dbfc6c5513709a6663f25fea93f04b4b8
Josh Zuech <josh.zuech@nextthought.com>
2017-03-10 23:12:14 +0000

    Much better target patch against mysql blob queries
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.monkey/trunk@108612 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/monkey/patch_relstorage_query.py
+++ src/nti/monkey/patch_relstorage_query.py
@@ -16,46 +16,18 @@ logger = __import__('logging').getLogger(__name__)
 from relstorage.iter import fetchmany
 
 
-def _nti_move_from_temp_object_state(self, cursor, tid):
-    """
-    Called for history-free databases.
-    Should replace all entries in object_state with the
-    same zoid from temp_store.
-    """
-    oid_stmt = """
-    SELECT zoid FROM temp_store
-    """
-    cursor.execute(oid_stmt)
-    oids = [oid for (oid,) in fetchmany(cursor)]
-    oids = ','.join(str(oid) for oid in oids)
-
-    stmt = """
-     DELETE FROM object_state
-     WHERE zoid IN (%s)
-     """
-    stmt = stmt % oids
-    cursor.execute(stmt)
-
-    stmt = self._move_from_temp_hf_insert_query
-    cursor.execute(stmt, (tid,))
-
-
 def _nti_move_from_temp(self, cursor, tid, txn_has_blobs):
-    """
-    Moved the temporarily stored objects to permanent storage.
-    Returns the list of oids stored.
-    """
-    oid_stmt = """
-    SELECT zoid FROM temp_store
-    """
+    oid_stmt = "SELECT zoid FROM temp_store"
 
     if self.keep_history:
         stmt = self._move_from_temp_hp_insert_query
         cursor.execute(stmt, (tid,))
     else:
-        _nti_move_from_temp_object_state(self, cursor, tid)
+        self._move_from_temp_object_state(cursor, tid)
 
         if txn_has_blobs:
+            # This is where we need to swap out the old
+            # relstorage subquery for this double query approach.
             cursor.execute(oid_stmt)
             oids = [oid for (oid,) in fetchmany(cursor)]
             oid_list = ','.join(str(oid) for oid in oids)
@@ -63,7 +35,8 @@ def _nti_move_from_temp(self, cursor, tid, txn_has_blobs):
         	DELETE FROM blob_chunk
         	WHERE zoid IN (%s)
         	"""
-            cursor.execute(stmt, (oid_list,))
+            stmt = stmt % oid_list
+            cursor.execute(stmt)
 
     if txn_has_blobs:
         stmt = self._move_from_temp_copy_blob_query
@@ -74,8 +47,7 @@ def _nti_move_from_temp(self, cursor, tid, txn_has_blobs):
 
 
 def patch():
-    from relstorage.adapters.mover import AbstractObjectMover
-    AbstractObjectMover.move_from_temp = _nti_move_from_temp
-    AbstractObjectMover._move_from_temp_object_state = _nti_move_from_temp_object_state
+    from relstorage.adapters.mysql.mover import MySQLObjectMover
+    MySQLObjectMover.move_from_temp = _nti_move_from_temp
 
 patch()
