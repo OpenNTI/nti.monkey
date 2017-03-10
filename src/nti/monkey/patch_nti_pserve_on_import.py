06165dfc4cbb43948b27a22b7427369fa3efa301
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-03-10 21:29:33 +0000

    add helper script
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.monkey/trunk@108594 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/monkey/patch_nti_pserve_on_import.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+
+from nti.monkey import patch_gevent_on_import
+patch_gevent_on_import.patch()
+
+from nti.monkey import patch_relstorage_all_on_import
+patch_relstorage_all_on_import.patch()
+
+from nti.monkey import patch_webob_cookie_escaping_on_import
+patch_webob_cookie_escaping_on_import.patch()
+
+from nti.monkey import patch_random_seed_on_import
+patch_random_seed_on_import.patch()
+
+from nti.monkey import patch_pyramid_on_import
+patch_pyramid_on_import.patch()
+
+from nti.monkey import patch_acquisition
+patch_acquisition.patch()
+
+
+def patch():
+    pass
