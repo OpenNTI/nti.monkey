a4d0186e2d081d3c454e95ca8b82029c2d4e1d9b
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-02-26 20:03:41 +0000

    remove patch_relstorage_query since we have a release of RelStorage that covers it
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.monkey/trunk@126687 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/monkey/patch_relstorage_all_on_import.py
+++ src/nti/monkey/patch_relstorage_all_on_import.py
@@ -6,24 +6,23 @@ Collects all the patches.
 .. $Id$
 """
 
-from __future__ import print_function, absolute_import, division
-__docformat__ = "restructuredtext en"
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
 
-logger = __import__('logging').getLogger(__name__)
-
-from nti.monkey import patch_relstorage_query
 from nti.monkey import patch_gevent_on_import
 from nti.monkey import patch_pyramid_on_import
 from nti.monkey import patch_nti_internal_on_import
 from nti.monkey import patch_relstorage_locker_on_import
 from nti.monkey import patch_relstorage_umysqldb_on_import
 
+logger = __import__('logging').getLogger(__name__)
+
 # Order matters
 patch_gevent_on_import.patch()
 patch_relstorage_umysqldb_on_import.patch()
 patch_pyramid_on_import.patch()
 patch_nti_internal_on_import.patch()
-patch_relstorage_query.patch()
 patch_relstorage_locker_on_import.patch()
 
 
