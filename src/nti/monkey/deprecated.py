301b451a730b2ac4de6bc522f3bba98ac908fa81
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-03-03 16:33:29 +0000

    minor
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.monkey/trunk@126814 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/monkey/deprecated.py
+++ src/nti/monkey/deprecated.py
@@ -4,14 +4,15 @@
 .. $Id$
 """
 
-from __future__ import print_function, absolute_import, division
-__docformat__ = "restructuredtext en"
-
-logger = __import__('logging').getLogger(__name__)
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
 
 import sys
 import types
 import warnings
+
+logger = __import__('logging').getLogger(__name__)
 
 
 def moved(from_location, to_location):
