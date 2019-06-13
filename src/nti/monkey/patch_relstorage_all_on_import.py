f40e0713e7cb7546f6e1b7c39737ed7a76e8138c
Josh Zuech <josh.zuech@nextthought.com>
2019-06-13 09:26:45 -0500

    Remove umysqldb patches

--- src/nti/monkey/patch_relstorage_all_on_import.py
+++ src/nti/monkey/patch_relstorage_all_on_import.py
@@ -14,14 +14,12 @@ from nti.monkey import patch_gevent_on_import
 from nti.monkey import patch_pyramid_on_import
 from nti.monkey import patch_nti_internal_on_import
 from nti.monkey import patch_relstorage_locker_on_import
-from nti.monkey import patch_relstorage_umysqldb_on_import
 from nti.monkey import patch_relstorage_allocator_on_import
 
 logger = __import__('logging').getLogger(__name__)
 
 # Order matters
 patch_gevent_on_import.patch()
-patch_relstorage_umysqldb_on_import.patch()
 patch_pyramid_on_import.patch()
 patch_nti_internal_on_import.patch()
 patch_relstorage_locker_on_import.patch()
