e182f7480e965819f68359e7395c1d5cbda7aa45
Josh Zuech <josh.zuech@nextthought.com>
2021-03-13 11:17:54 -0600

    Remove copied comment

--- src/nti/monkey/patch_site_registry_on_import.py
+++ src/nti/monkey/patch_site_registry_on_import.py
@@ -1,11 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-Python versions prior to 2.7.7 do a very poor job seeding
-the random number space. This backport the algorithm from 2.7.7.
-
-.. $Id$
-"""
 
 from __future__ import print_function, absolute_import, division
 
